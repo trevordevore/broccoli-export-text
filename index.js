8a8cddf1a20196b1d2e8e7776304cfdf2322c2f7
Trevor DeVore <trevor@screensteps.com>
2021-01-30 12:30:36 -0600

    Get working with modern broccoli

--- index.js
+++ index.js
@@ -1,55 +1,52 @@
 'use strict';
 
+var Plugin = require('broccoli-plugin');
 var fs = require('fs');
 var path = require('path');
-var Plugin = require('broccoli-plugin');
 var glob = require('glob');
 var jsesc = require('jsesc');
 var mkdirp = require('mkdirp');
 
-// Create a subclass Glimmer derived from Plugin
-function BrocText (inputTree, options) {
-  options || (options = {});
+module.exports = class BrocText extends Plugin {
+  targetExtension = 'js';
 
-  this.options = options;
-  this.jsescOptions = options.jsesc || {};
-  this.jsescOptions.quotes = this.jsescOptions.quotes || 'single';
-  this.jsescOptions.wrap = this.jsescOptions.wrap || 'wrap';
+  constructor(node, _options = {}) {
+    super([node], {
+      name: _options.name,
+      annotation: _options.annotation,
+      persistentOutput: true
+    });
 
-  if (options.extensions) {
-    options.extensions = [].concat(options.extensions);
-  } else {
-    console.log('No "extensions" option defined for Broccoli Text. Defaulting to "txt" extensions.');
-    options.extensions = ['txt'];
+    let options = _options || {};
+
+    this.options = options;
+    this.jsescOptions = options.jsesc || {};
+    this.jsescOptions.quotes = this.jsescOptions.quotes || 'single';
+    this.jsescOptions.wrap = this.jsescOptions.wrap || 'wrap';
+
+    if (options.extensions) {
+      options.extensions = [].concat(options.extensions);
+    } else {
+      console.log('No "extensions" option defined for Broccoli Text. Defaulting to "txt" extensions.');
+      options.extensions = ['txt'];
+    }
   }
 
-  Plugin.call(this, inputTree, {});
+  build() {
+    return new Promise((resolve, reject) => {
+      glob("**/*."+this.options.extensions, {cwd: this.inputPaths[0]}, (err, files) => {
 
-}
+        if (err) { return reject(err); }
 
-BrocText.prototype = Object.create(Plugin.prototype);
+        files.forEach((file) => {
+          var inputBuffer = fs.readFileSync(path.join(this.inputPaths[0], file)).toString('utf8');
+          var out = jsesc(inputBuffer, this.jsescOptions);
+          mkdirp.sync(path.dirname(path.join(this.outputPath, file)));
+          fs.writeFileSync(path.join(this.outputPath, file).replace('.'+this.options.extensions, '.js'), 'export default ' + out + ';');
+        });
 
-BrocText.prototype.constructor = BrocText;
-
-BrocText.prototype.targetExtension = 'js';
-
-BrocText.prototype.build = function build(){
-
-  return new Promise((resolve, reject) => {
-    glob("**/*."+this.options.extensions, {cwd: this.inputPaths[0]}, (err, files) => {
-
-      if (err) { return reject(err); }
-
-      files.forEach((file) => {
-        var inputBuffer = fs.readFileSync(path.join(this.inputPaths[0], file)).toString('utf8');
-        var out = jsesc(inputBuffer, this.jsescOptions);
-        mkdirp.sync(path.dirname(path.join(this.outputPath, file)));
-        fs.writeFileSync(path.join(this.outputPath, file).replace('.'+this.options.extensions, '.js'), 'export default ' + out + ';');
+        resolve()
       });
-
-      resolve()
     });
-  });
-};
-
-module.exports = BrocText;
+  }
+}
